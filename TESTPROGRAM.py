241a613ff4d1d4f8197e72f269c602417569962e
Quebada-1CPE-A <luizareginoquebada123@gmail.com>
2026-06-25 08:47:01 +0800

    Add files via upload

---
+++ TESTPROGRAM.py
@@ -0,0 +1,25 @@
+print("Hello, World")
+n = 9
+if 0>n:
+    print("n is negative")
+else:
+    print("n is positive or zero")
+
+score = 86
+if score >= 98:
+    print("With Highest Honors")
+elif score >= 95:
+    print("With High Honors")
+elif score >= 90:
+    print("With Honors")
+elif score >= 85 and score < 90:
+    print("Achievers")
+
+a = "I Heart PnC"
+print(a[3])
+
+
+
+
+# Test for mixing text & numbers
+print("If you add", 33, "and", 34, "You get", 33+34)
